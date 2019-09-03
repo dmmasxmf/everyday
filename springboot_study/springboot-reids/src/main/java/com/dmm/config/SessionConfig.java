7a6dbb11106eeab5a711a61006a783a2e7b229a5
dmm <15738669005@139.com>
2019-09-03 19:18:33 +0800

    增加spring-redis等使用

---
+++ springboot_study/springboot-reids/src/main/java/com/dmm/config/SessionConfig.java
@@ -0,0 +1,27 @@
+package com.dmm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author DMM
+ * @create 2019/9/3
+ */
+/**
+ *
+ * maxInactiveIntervalInSeconds 最大无效间隔秒
+ *
+ * 设置Session失效时间，使用Redis Session之后 默认1800s
+ * #session更新策略，有ON_SAVE、IMMEDIATE，前者是在调用#SessionRepository#save(org.springframework.session.Session)时，
+ * 在response commit前刷新缓存，#后者是只要有任何更新就会刷新缓存
+ *
+ * cleanupCron() default "0 * * * * *";
+ * 清空的cron表达式
+ */
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60, redisFlushMode = RedisFlushMode.IMMEDIATE, redisNamespace = "session" )
+public class SessionConfig {
+}
+
---
+++ springboot_study/springboot-reids/src/main/java/com/dmm/service/UserService.java
@@ -0,0 +1,15 @@
+package com.dmm.service;
+
+import com.dmm.entry.User;
+
+/**
+ * @author DMM
+ * @create 2019/9/3
+ */
+
+public interface UserService {
+
+    String modifyUser(User user);
+
+}
+
