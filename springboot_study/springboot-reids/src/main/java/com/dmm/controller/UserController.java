42fa90edac2cc1e58b918f4a5ed1b969a67b9878
dmm <15738669005@139.com>
2019-09-04 19:11:16 +0800

    测试日志

--- springboot_study/springboot-reids/src/main/java/com/dmm/controller/UserController.java
+++ springboot_study/springboot-reids/src/main/java/com/dmm/controller/UserController.java
@@ -2,6 +2,7 @@ package com.dmm.controller;
 
 import com.dmm.entry.User;
 import com.dmm.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,17 +15,29 @@ import javax.servlet.http.HttpSession;
  * @create 2019/9/3
  */
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/test")
-    public String modify(@RequestBody User user, HttpSession httpSession){
+    public String modify(@RequestBody User user, HttpSession httpSession) {
         //Cookie cookie=new Cookie("ee","啊啊啊");
         //httpServletResponse.addCookie(cookie);
         //httpSession.setAttribute("dmm","最漂亮");
+//        try {
+//            throw new Exception("我是错误");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            log.error("{}",e);
+//        }
+
         return userService.modifyUser(user);
+
+        //System.out.println(1/0);
+
+        //return userService.test();
     }
 }
 
