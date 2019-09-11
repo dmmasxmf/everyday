bedc4b56d2d086c8d1bcfd2ae67781a8320353b9
dmmasxmf <15738669005@139.com>
2019-09-11 16:31:24 +0800

    没有主清单属性

--- springboot_study/springboot_rabbitmq/src/main/java/com/dmm/config/TopicConfig.java
+++ springboot_study/springboot_rabbitmq/src/main/java/com/dmm/config/TopicConfig.java
@@ -1,5 +1,5 @@
-package com.dmm.config;//package com.dmm.config;
-//
+package com.dmm.config;
+
 //import org.springframework.amqp.core.Queue;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
